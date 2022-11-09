a4428ff0d365f17d6c3108ba2a9dd54ae0c93e8d
Flavia-Duran <duranpepeu@gmail.com>
2022-11-09 19:40:02 -0300

    Definindo número de tentativas através de while

--- jogoAdivinhacao/adivinhacao.py
+++ jogoAdivinhacao/adivinhacao.py
@@ -3,13 +3,28 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
-chute_str = input("Digite o seu numero: ")
+rodada = 1
+total_de_tentativas = 3
 
-print("Você digitou", chute_str)
+while (rodada <= total_de_tentativas):
+    print("Tentativa",
+    rodada, "de", total_de_tentativas)
+    chute_str = input("Digite o seu numero: ")
+    print("Você digitou", chute_str)
 
-numero = int(chute_str)
+    chute = int(chute_str)
 
-if (numero_secreto == numero):
-    print("você acertou!")
-else:
-    print("você errou!")
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("você acertou!")
+    else:
+
+        if (maior):
+            print("você errou! O seu chute foi maior do que o número secreto")
+        elif (menor):
+            print("Você errou! O seu chute foi menor do que o número secreto")
+
+    rodada = rodada + 1
